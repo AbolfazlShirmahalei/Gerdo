799900339070b1524a982b2c5507190d1068198d
abolfazl <abolfazl.shirmahalei@tapsi.cab>
2023-10-19 15:38:49 +0330

    utils was created and Database class was added to it

---
+++ utils/database/application.py
@@ -0,0 +1,24 @@
+import sqlite3
+from typing import Dict
+
+import pandas as pd
+
+from utils.database.config import DATABASE_NAME
+
+
+class Database:
+    def __init__(
+        self,
+        data_name_to_address: Dict[str, str],
+        data_base_name: str = DATABASE_NAME,
+    ):
+        connection = sqlite3.connect(data_base_name)
+
+        for table_name, data_address in data_name_to_address.items():
+            data = pd.read_csv(data_address)
+            data.to_sql(table_name, con=connection, if_exists="append")
+
+        self.cursor = connection.cursor()
+
+    def query(self, my_query: str):
+        return self.cursor.execute(my_query, ).fetchall()
