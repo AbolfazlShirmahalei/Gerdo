1a6796e93d53be378bcc3e4a6a201731114f5847
abolfazl <abolfazl.shirmahalei@tapsi.cab>
2023-10-19 15:47:49 +0330

    config file was created and data paths were added to it

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+from .data_path import (
+    BUG_PATH,
+    CAMPAIGN_PATH,
+    CUSTOMERS_PATH,
+    GRAPH_MINING_PATH,
+    LOGIN_PATH,
+    TRX_PATH,
+)
---
+++ config/data_path.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_PATH = "/home/tapsi/Downloads/Abolfazl"
+
+BUG_PATH = os.path.join(BASE_PATH, "bug.csv")
+CAMPAIGN_PATH = os.path.join(BASE_PATH, "campaign.csv")
+CUSTOMERS_PATH = os.path.join(BASE_PATH, "customers.csv")
+GRAPH_MINING_PATH = os.path.join(BASE_PATH, "graph_mining.csv")
+LOGIN_PATH = os.path.join(BASE_PATH, "login.csv")
+TRX_PATH = os.path.join(BASE_PATH, "trx.csv")
